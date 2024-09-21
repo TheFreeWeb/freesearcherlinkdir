29d04664bcd54a6b5cc8c4f0a96136c162bacb28
Woo <150756291+WooRepo@users.noreply.github.com>
2024-09-21 15:25:32 +1200

    Next 12 done!

---
+++ search-manga-one-piece.volumes-baroque-works.bat
@@ -0,0 +1,13 @@
+echo This will download the volumes 13–23 of "One Piece". Do you wish to proceed?
+CHOICE /C YNC /M "Press Y for Yes, N for No or C for Cancel"curl -O https://files.catbox.moe/gh5ll4.cbz
+curl -O https://files.catbox.moe/b2kcta.cbz
+curl -O https://files.catbox.moe/atcuik.cbz
+curl -O https://files.catbox.moe/f1cymg.cbz
+curl -O https://files.catbox.moe/4nm3w8.cbz
+curl -O https://files.catbox.moe/c6ibm2.cbz
+curl -O https://files.catbox.moe/i0xbkd.cbz
+curl -O https://files.catbox.moe/9xwhev.cbz
+curl -O https://files.catbox.moe/rqd5ne.cbz
+curl -O https://files.catbox.moe/czuns8.cbz
+curl -O https://files.catbox.moe/m0lc3c.cbz
+echo All done!
